9c0dd0d7c7468b5077c9c2419b756c37bc89c38e
wizgurl101 <nb.phan@hotmail.com>
2024-12-18 20:21:54 -0700

    add menu bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import RootLayout from "@/app/layout";
+import './global.css'
+import MenuBar from "@/components/MenuBar/page";
 
 export default function Home() {
   return (
       <RootLayout>
+          <MenuBar />
         <div>Home Page</div>
       </RootLayout>
   );
